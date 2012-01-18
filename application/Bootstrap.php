056cfd0b64619beb2654778bf663c1addb7328ba
Nully <nully.nl@gmail.com>
2012-01-18 18:34:15 +0900

    表示に関する起動コードを追記

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,32 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    /**
+     * Doctypeの設定
+     *
+     */
+    protected function _initDoctype()
+    {
+        $view = $this->bootstrap("view")->getResource("view");
+        $view->doctype("XHTML1_TRANSITIONAL");
+    }
 
 
+    /**
+     * レイアウトで利用するCSSファイルやJavaScriptファイルの出力準備を行う
+     *
+     */
+    protected function _initLayoutResources()
+    {
+        $view = $this->bootstrap("view")->getResource("view");
+        $view->headLink(array(
+            "rel"   => "stylesheet",
+            "href"  => $view->baseUrl("/css/import.css"),
+            "type"  => "text/css",
+            "media" => "screen,projection,tv",
+        ));
+
+        $view->headTitle()->setSeparator(" | ")->append("Jobeet For ZendFramework");
+    }
 }
 
